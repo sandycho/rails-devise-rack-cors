4395b8b970c629a6ccda050545e510833d573513
Sandy Chuchuca <latagliatella@leadclic.com>
2015-09-02 17:02:19 +0200

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,15 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  before_action :authenticate_user!, except: :home
+  
+  def home
+  end
+  
+  # GET 'application#getUser'
+  def getUser
+    @user = User.new(email: 'usertest0@test.com')
+  end
+end
