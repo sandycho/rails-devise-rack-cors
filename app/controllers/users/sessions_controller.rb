a07adab3e66fcb902eae3505b9cca0e1891ee8c4
ladyshirka <ladyshirka@gmail.com>
2015-09-03 21:28:38 +0000

    update

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,6 +6,15 @@ class Users::SessionsController < Devise::SessionsController
   def new
     super
   end
+  
+  def create
+    if request.xhr?
+      resource = warden.authenticate!(:scope => resource_name, :recall => "sessions#failure")
+      return sign_in_and_redirect(resource_name, resource)
+    else
+      super
+    end
+  end
 
   # POST /resource/sign_in
   # def create
