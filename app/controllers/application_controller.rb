f8fb07300241b219787f8c1a9043f6a12755f971
ladyshirka <ladyshirka@gmail.com>
2015-09-03 21:18:00 +0000

    Añadido el filtro para comprobar el tipo de conexión

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,27 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :is_xrh_request?
+  
   protect_from_forgery with: :exception
   skip_before_filter :verify_authenticity_token
   
   before_action :authenticate_user!, except: [:home, :getUnknownUser]
   
   respond_to :json, :html
+  
+  def is_xrh_request?
+    is_hr = request.xml_http_request?
+    puts 'REQUEST TYPE: ' + is_hr.to_s
+    
+    if is_hr
+      render json: {data: 'is a xhr'}
+    else
+      
+      render json: {data: 'is not a xhr'}  
+    end
+  end
+  
   def home
   end
   
